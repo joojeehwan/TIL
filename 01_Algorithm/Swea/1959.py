5128b600350c80dd6abb08e12e1c00e535f15ab7
joojeehwan <meanstrike94@gmail.com>
2021-07-23 02:16:47 +0900

    20210722_04_ShortestPath

---
+++ 01_Algorithm/Swea/1959.py
@@ -0,0 +1,20 @@
+
+
+
+"""
+1959: 두 개의 숫자열
+"""
+
+import sys
+sys.stdin = open('1959_input.txt')
+
+
+
+T = int(input())
+
+
+for tc in range(1, T+1):
+    pass
+
+
+
---
+++ 01_Algorithm/Swea/2001.py
@@ -0,0 +1,20 @@
+"""
+2001: 파리 퇴치
+"""
+
+import sys
+sys.stdin = open('2001_input.txt')
+
+
+print(sys.stdin)
+
+
+
+
+
+
+
+
+
+
+
