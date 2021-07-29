8e56e24fd882babe22a5d7f2f75d2e26424733ee
joojeehwan <meanstrike94@gmail.com>
2021-07-30 02:23:17 +0900

    20210729-swea.1959

--- 01_Algorithm/Swea/1959.py
+++ 01_Algorithm/Swea/1959.py
@@ -1,20 +1,40 @@
 
 
-
 """
-1959: 두 개의 숫자열
+1959: 
 """
 
 import sys
 sys.stdin = open('1959_input.txt')
 
 
+def multiply_max(lst1, lst2):
 
-T = int(input())
+    maxi = 0
+    for i in range(len(lst2) - len(lst1) + 1):
+        temp = 0
+
+        for j in range(len(lst1)):
+            temp += lst1[j] * lst2[i + j]
+        if maxi < temp:
+            maxi = temp
+    return maxi
 
 
-for tc in range(1, T+1):
-    pass
+test_case = int(input())
 
 
+for tc in range(1, test_case + 1):
+    first, second = map(int, input().split())
+
+    first_lst = list(map(int, input().split()))
+
+    second_lst = list(map(int, input().split()))
+
+    if first > second:
+        ans = multiply_max(second_lst, first_lst)
+    else:
+        ans = multiply_max(first_lst, second_lst)
+
+    print(f'#{tc} {ans}')
 
