ca5f87692ee1dd6aed06af778299c572238ebf3e
joojeehwan <meanstrike94@gmail.com>
2021-09-09 00:12:47 +0900

    0909_algo

---
+++ 01_Algorithm/baekjoon/1357.py
@@ -0,0 +1,28 @@
+'''
+
+
+뒤집힌 덧셈
+
+형변환
+
+
+'''
+
+
+
+def change_rev(a):
+
+    x = list(map(int, a.rstrip()))
+
+    x_temp = x[::-1]
+
+    x_rev = int("".join(map(str, x_temp)))
+
+    return x_rev
+
+
+x, y = input().split()
+
+res = change_rev(str(change_rev(x) + change_rev(y)))
+
+print(res)
