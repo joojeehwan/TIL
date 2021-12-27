080d462d9e98e7a8d6216c044f93a717dcd78df2
joojeehwan <meanstrike94@gmail.com>
2021-12-27 23:23:54 +0900

    1227 movie clear

--- 09_Front/movie_react_begin/src/App.js
+++ 09_Front/movie_react_begin/src/App.js
@@ -1,53 +1,159 @@
 
-import { useEffect, useState } from "react"
 
-
-
-function Hello () {
-  // function byFn() {
-  //   console.log("bye")
-  // }
-
-  // function hiFn () {
-  //   console.log("created")
-  //   return byFn
-  // }
-  // useEffect(hiFn,[])
-  // useEffect(() => {
-  //   console.log("i'm created")
-  //   return () => console.log("destoryed")
-  // }, [])
-
-  // useEffect(() => {
-  //     console.log("hi")
-  //   return () => console.log("bye")
-  // },[])
-
-  useEffect(function() {
-    console.log("hi")
-    return function() {
-      console.log("bye")
-    }
-  },[])
-  return <h1>Hello</h1>
-}
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home"
 
 function App() {
-
-  const [showing, setShowing] = useState(false)
-  const onClick = () => setShowing((prev) => !prev)
-
-  return ( 
-  <div>
-    {showing ? <Hello /> : null}
-    <button onClick={onClick}> {showing ? "Hide" : "showing"} </button>
-
-  </div>
+  return (
+  <Router>
+    <Switch>
+      <Route path="/movie/:id">
+        <Detail/>
+      </Route>
+      <Route path="/">
+        <Home/>
+      </Route>
+    </Switch>
+  </Router>
   )
+}
+
+//-----------------------------코인 트래커
+
+// function App() {
+
+//   const [loading, setLoading] = useState(true)
+//   const [coins, setCoins] = useState([])
+//   useEffect(() => {
+//       fetch("https://api.coinpaprika.com/v1/tickers")
+//         .then((response) => response.json())
+//         .then((json) => {
+//           setCoins(json)
+//           setLoading(false)
+//         })
+//   }, [])
+//   return ( 
+//     <div>
+//         <h1>The Coins ({coins.length})</h1>
+//         {loading ? <strong>Loading...</strong> : null}
+      
+//         <ul>
+//           {coins.map((coin) => (
+
+//             <li>
+//               {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+//             </li>
+//           ))}
+          
+//         </ul>
+//     </div> 
+//   )
+// }
 
 
 
-///-------------------------------------------
+
+
+
+//----------------todolist
+
+// function App() {
+
+
+//   const [todo, setTodo] = useState("")
+//   const [toDos, setToDos] = useState([])
+//   const onChange = (event) => setTodo(event.target.value)
+//   const onSubmit = (event) => {
+//     event.preventDefault()
+//     if (todo === "") {
+//       return
+//     }
+//     //기존의 것 + 새로운 값 배열에 추가하는 js
+//     setToDos((currentArray) => [todo, ...currentArray])
+//     setTodo("")
+//   }
+ 
+//   console.log(toDos)
+//   return (
+//     <div>
+//       <h1>My To Dos ({toDos.length})</h1>
+//       <form onSubmit={onSubmit}>
+//         <input 
+//         onChange={onChange}
+//         value={todo} 
+//         type="text" 
+//         placeholder="Write your to do,,,"
+//         />
+//         <button>Add To do</button>
+//       </form>
+//       <hr/>
+
+//       <ul>
+//         {toDos.map((item, index) => (
+//           <li key={index}>{item}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+// 1226 (2)clean up
+
+// function Hello () {
+//   // function byFn() {
+//   //   console.log("bye")
+//   // }
+
+//   // function hiFn () {
+//   //   console.log("created")
+//   //   return byFn
+//   // }
+//   // useEffect(hiFn,[])
+//   // useEffect(() => {
+//   //   console.log("i'm created")
+//   //   return () => console.log("destoryed")
+//   // }, [])
+
+//   // useEffect(() => {
+//   //     console.log("hi")
+//   //   return () => console.log("bye")
+//   // },[])
+
+//   useEffect(function() {
+//     console.log("hi")
+//     return function() {
+//       console.log("bye")
+//     }
+//   },[])
+//   return <h1>Hello</h1>
+// }
+
+// function App() {
+
+//   const [showing, setShowing] = useState(false)
+//   const onClick = () => setShowing((prev) => !prev)
+
+//   return ( 
+//   <div>
+//     {showing ? <Hello /> : null}
+//     <button onClick={onClick}> {showing ? "Hide" : "showing"} </button>
+
+//   </div>
+//   )
+
+
+
+///1226 (1) useEffect
 
   // const [counter, setValue] = useState(0)
   // const [keyword, setKeyword] = useState("")
@@ -84,7 +190,7 @@ function App() {
   //     <button onClick={onClick}>click me</button>  
   //   </div>
   // )
-}
+//}
 
 export default App;
  
---
+++ 09_Front/movie_react_begin/src/components/MovieDetail.js
@@ -0,0 +1,16 @@
+function MovieDtail({rating,year,description_full}) {
+  return (
+
+      <div>
+
+        <h2>{rating}</h2>
+        <h2>{year}</h2>
+        <p>{description_full}</p>
+
+      </div>
+
+    
+  )
+}
+
+export default MovieDtail
