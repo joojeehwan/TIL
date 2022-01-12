07ad73198da9ba20422eef1cf3a099fa313424c3
jeehwan <meanstrike94@gmai.com>
2022-01-12 21:11:57 +0900

    0112 ssafy

---
+++ 09_Front/react_redux/redux/src/index.js
@@ -0,0 +1,62 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add")
+const minus = document.getElementById("minus")
+const number = document.querySelector("span")
+
+// const countModifer = (count = 0) => {
+
+//   return count
+// }
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+number.innerText = 0
+const countModifier = (count = 0, action) => {
+
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState()
+}
+countStore.subscribe(onChange)
+
+add.addEventListener("click", () => countStore.dispatch({type:ADD}))
+minus.addEventListener("click", () => countStore.dispatch({type:MINUS}))
+
+
+
+
+
+
+// const updateText = () => {
+//   number.innerText = count
+//   updateText()
+// }
+
+// const handleAdd = () => {
+//   count = count += 1
+//   updateText()
+// }
+
+// const handleminus = () => {
+//   count = count -= 1
+//   updateText()
+// }
+
+
+
+// add.addEventListener("click", handleAdd)
+// minus.addEventListener("click", handleminus)
